bf288e48a7f6b1519b2e42d42cab1ddf46ecc787
chenchong <chenchong1004@163.com>
2019-04-01 14:48:29 +0800

    edit in branch hot_fix

--- src/main/java/git/Hello.java
+++ src/main/java/git/Hello.java
@@ -2,8 +2,7 @@ package git;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("left......");
-		System.out.println("right.......");
+		System.out.println("hot_fix......");
 	}
 	
 	
